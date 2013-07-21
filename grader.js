3b1144200c2065bfa2a7a79ce5b1037a76676472
Stephen Opoku Agyeman <stephen.agyeman@gmail.com>
2013-07-21 03:42:21 +0000

    updated grader.js

--- grader.js
+++ grader.js
@@ -66,7 +66,7 @@ if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
-        .option('-u, --url <url_address>', 'url of webpage', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url_address>', 'url of webpage')
         .parse(process.argv);
     var checkJson;
     if (fs.existsSync(program.file)){
